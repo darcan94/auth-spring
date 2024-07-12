62c6f2fc32d25dc4a1cb734a92d83d05f98ce0fe
Dario Candia <darcanf.94@gmail.com>
2024-07-11 22:13:47 -0400

    Disable csrf

--- src/main/java/com/darcan/auth/config/SecurityConfig.java
+++ src/main/java/com/darcan/auth/config/SecurityConfig.java
@@ -2,7 +2,9 @@ package com.darcan.auth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,9 +13,13 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http
-            .authorizeHttpRequests()
-            .anyRequest()
-            .permitAll();
+            .authorizeHttpRequests(customizeRequests -> {
+                customizeRequests
+                    .anyRequest()
+                    .authenticated();
+            })
+            .csrf(AbstractHttpConfigurer::disable)
+            .httpBasic(Customizer.withDefaults());
             
         return http.build();
     }
